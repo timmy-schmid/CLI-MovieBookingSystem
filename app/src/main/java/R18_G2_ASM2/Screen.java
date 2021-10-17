c029fa90e95c37e8d0473048f8277851c539cc2b
Tim <tsch5702@uni.sydney.edu.au>
2021-10-17 18:42:35 +1100

    added movie.csv,showing.csv,cinema.csv

--- app/src/main/java/R18_G2_ASM2/Screen.java
+++ app/src/main/java/R18_G2_ASM2/Screen.java
@@ -1,17 +1,22 @@
 package R18_G2_ASM2;
+public class Screen {
 
-public enum Screen {
-  GOLD(2),
-  SILVER(1.5),
-  BRONZE(1);
-
-  private final double multiplier;
-
-  private Screen(double multiplier) {
-    this.multiplier = multiplier;
+  public static void printBar(StringBuilder s) {
+    s.append("-------------------------------------------------------\n");
   }
 
-  public double getMultiplier () {
-    return multiplier;
+  public static void printHeader(StringBuilder s) {
+    s.append("-------------------------------------------------------\n");
+    s.append("ID   MOVIE                             TIME     CINEMA\n");
+    s.append("-------------------------------------------------------\n");
   }
+
+  //public static void printMovieRow(StringBuilder s, Movie m) {
+    //printBar(s);
+    //s.append(String.format("%4d",f.id));
+    //s.append(String.format("%10s",f.departureTime.toString(TextStyle.SHORT)));
+    //printBar(s);
+  //}
+
+
 }
