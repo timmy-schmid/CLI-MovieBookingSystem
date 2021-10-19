5467d0df68168d06d9664e59e9a7352a4a24d78d
Junbin Gou <jgou4809@uni.sydney.edu.au>
2021-10-20 00:44:12 +0800

    Update App.java

--- app/src/main/java/R18_G2_ASM2/App.java
+++ app/src/main/java/R18_G2_ASM2/App.java
@@ -8,7 +8,7 @@ public class App {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Login login = new Login();
         login.retrieveUserInputDetails();
     }
