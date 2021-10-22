d76c49f49081e17024ff93080979539c33cf3460
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-10-22 17:56:44 +0800

    editing information

--- app/src/main/java/R18_G2_ASM2/EditInformation.java
+++ app/src/main/java/R18_G2_ASM2/EditInformation.java
@@ -64,7 +64,7 @@ public class EditInformation extends UserFields {
         System.out.println("The new username: ");
         while(!Success && wantToContinue){
             //not sure about where the code comes from and how to verify the code is correct one
-            String option = scan.nextLine();
+            String option = scan.next();
             if(option.equals(this.userChanged.getEmail())){
                 System.out.println("Please enter a new username");
             }
