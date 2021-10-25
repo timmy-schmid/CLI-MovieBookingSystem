39e0b3e9b7ce2c69cf8dbccb608968546e58e9e2
Junbin Gou <jgou4809@uni.sydney.edu.au>
2021-10-25 21:49:01 +0800

    change last one username and password

--- app/src/main/java/R18_G2_ASM2/App.java
+++ app/src/main/java/R18_G2_ASM2/App.java
@@ -5,7 +5,7 @@ package R18_G2_ASM2;
 
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         MovieSystem system = new MovieSystem();
         system.run();
     }
