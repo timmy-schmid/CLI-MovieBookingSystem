330722612765d2f101e0f3bf12121fe516842c8f
Wei Zhang <wzha2184@uni.sydney.edu.au>
2021-10-26 16:38:40 +0800

    add freatures in showing

--- app/src/main/java/R18_G2_ASM2/MovieSeat.java
+++ app/src/main/java/R18_G2_ASM2/MovieSeat.java
@@ -21,7 +21,7 @@ public class MovieSeat{
     public MovieSeat(Showing showing) throws IOException{
         this.showing = showing;
 
-        movieSeat = new File("src/main/datasets/movieSeatsMap/"+ String.valueOf(showing.getMovie().getId())+"-"+ String.valueOf(showing.getCinema().getId())+"-"+String.valueOf(showing.getShowingId())+".csv");
+        movieSeat = new File("app/src/main/datasets/movieSeatsMap/"+ String.valueOf(showing.getMovie().getId())+"-"+ String.valueOf(showing.getCinema().getId())+"-"+String.valueOf(showing.getShowingId())+".csv");
         // movieSeat = new File("app/src/main/datasets/movieSeatsMap/BRONZE.csv");
 
         if (!movieSeat.exists()){
@@ -154,6 +154,48 @@ public class MovieSeat{
         return totalFrontSeat+totalMiddleSeat+totalRearSeat-totalSeatsBooked();
     }
 
+
+    public static void main(String[] args) throws IOException{
+        List<String> colNames;
+        // colNames = Arrays.asList("col_1", "col_2", "col_3", "col_4", "col_5", "col_6", "col_7");
+        colNames = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
+        String[][] data = { { "Available", "Available",  "Available", "Available", "Available", "Available", "Available" },
+                { "Reserved", "Available", "Available", "Available", "Available", "Available", "Available"},
+                { "Available", "Available", "Available", "Reserved", "Available" , "Available", "Available"},
+                { "Available", "Available", "Available", "Reserved", "Reserved" , "Available", "Available"},
+                { "Available", "Available", "Available", "Available", "Reserved" , "Available", "Available"},
+                { "Available", "Available", "Available", "Available", "Reserved" , "Available", "Available"}};
+        MovieDataFrame filmAvailable = new MovieDataFrame(colNames, data);
+        // filmAvailable.print(0,4);
+    
+        // System.out.println(filmAvailable.getRow(1).getValues());
+    
+    
+        System.out.println("\033[1;93;45m"+ "hello"+"\033[m");
+    
+        // mdf.print();
+        // File movieSeat = new File("src/test/resources/"+ "SeatMapTest.csv");
+    
+        MovieSeat seatMap = new MovieSeat(new Showing(2, new Movie(1,"77", null, null, null, null,null), new Cinema(1, Screen.SILVER), null));
+        DataFrame<String> newFrame = seatMap.readFromDatabase();
+        seatMap.writeToDatabase();
+        // newFrame.print();
+
+        seatMap.bookSeat('D', 5);
+    
+
+        seatMap.showFrontSeats();
+        seatMap.showMiddleSeats();
+        seatMap.showRearSeats();
+
+        seatMap.showAllSeats();
+
+
+        System.out.println("Front seats booked: "+seatMap.frontSeatBooked());
+        System.out.println("Middle seats booked: "+seatMap.middleSeatBooked());
+        System.out.println("Total seats left: "+seatMap.totalSeatsLeft());
+        System.out.println("Total seats booked: "+seatMap.totalSeatsBooked());
+    }
 
     
 }
