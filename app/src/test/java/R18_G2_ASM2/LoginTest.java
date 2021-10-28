aa0653534d1822c03d76b2368a4df0e4a0578150
Junbin Gou <jgou4809@uni.sydney.edu.au>
2021-10-28 16:25:09 +0800

    logintests

--- app/src/test/java/R18_G2_ASM2/LoginTest.java
+++ app/src/test/java/R18_G2_ASM2/LoginTest.java
@@ -57,42 +57,38 @@ class LoginTest {
     assertNull(login.getUser());
   }
 
-  // @Test
-  // public void testValidUsername(){
-  //   Login login = new Login();
-  //   String username = "dannie@gmail.com";
-  //   String password = "Asdf1234!*";
-  //   // int result = login.checkIfUserExists(username, password);
-  //   int result = login.checkIfUserExists2(username, password);
-  //   assertEquals(result, 1); //1 = user exists  (match found)
-  // }
+   @Test
+   public void testValidUsername(){
+     String username = "dannie@gmail.com";
+     String password = "Asdf1234!*";
+     // int result = login.checkIfUserExists(username, password);
+     int result = login.checkIfUserExists2(username, password);
+     assertEquals(result, 1); //1 = user exists  (match found)
+   }
 
-  // @Test
-  // public void testInvalidUsername(){
-  //   Login login = new Login();
-  //   String username = "dann@gmail.com";
-  //   String password = "Asdf1234!*";
-  //   int result = login.checkIfUserExists2(username, password);
-  //   assertEquals(result, -1); // no match found
-  // }
+   @Test
+   public void testInvalidUsername(){
+     String username = "dann@gmail.com";
+     String password = "Asdf1234!*";
+     int result = login.checkIfUserExists2(username, password);
+     assertEquals(result, -1); // no match found
+   }
 
-  // @Test
-  // public void testValidPassword(){
-  //   Login login = new Login();
-  //   String username = "harrypotter@gmail.com";
-  //   String password = "Wutttt123Asdf!*";
-  //   int result = login.checkIfUserExists2(username, password);
-  //   assertEquals(result, -1);
-  // }
+   @Test
+   public void testValidPassword(){
+     String username = "harrypotter@gmail.com";
+     String password = "Wutttt123Asdf!*";
+     int result = login.checkIfUserExists2(username, password);
+     assertEquals(result, -1);
+   }
 
-  // @Test
-  // public void testInvalidPassword(){
-  //   Login login = new Login();
-  //   String username = "harrypotter@gmail.com";
-  //   String password = "Asdf1235!";
-  //   int result = login.checkIfUserExists2(username, password);
-  //   assertEquals(result, -1);
-  // }
+   @Test
+   public void testInvalidPassword(){
+     String username = "harrypotter@gmail.com";
+     String password = "Asdf1235!";
+     int result = login.checkIfUserExists2(username, password);
+     assertEquals(result, -1);
+   }
 
   @Test
   public void testPrintScreen(){
