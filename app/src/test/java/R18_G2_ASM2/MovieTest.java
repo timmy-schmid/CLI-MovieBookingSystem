1305331cdeebe14c529c68257844bb8d692771c9
Tim <tsch5702@uni.sydney.edu.au>
2021-10-29 19:45:08 +1100

    added Movie Test for printing movie details

--- app/src/test/java/R18_G2_ASM2/MovieTest.java
+++ app/src/test/java/R18_G2_ASM2/MovieTest.java
@@ -3,6 +3,9 @@ package R18_G2_ASM2;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,16 +17,31 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class MovieTest {
 
-  static HashMap<Integer,Movie> movies;
-  final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+  private HashMap<Integer,Movie> movies;
+  private HashMap<Integer,Cinema> cinemas;
+  private ArrayList<Showing> showings;
+  private final DateFormat df = new SimpleDateFormat("yyyy-MM-dd",Locale.ENGLISH);
+  private final DateFormat df2 = new SimpleDateFormat("EEE dd MMM - K:mma",Locale.ENGLISH);
+  private static ByteArrayOutputStream actualOut;
+  ArrayList<Calendar> showingTime;
 
-  @BeforeAll static void setupTestData() {
-    
+  @BeforeAll static void setup() {
+    actualOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(actualOut));
+  }
+  
+  @BeforeEach void setupTestData() {
+    actualOut.reset(); 
     movies = new HashMap<>();
+    cinemas = new HashMap<>();
+    showings = new ArrayList<>();
+
     final TimeZone AEST = TimeZone.getTimeZone("Australia/Sydney");
+    df.setTimeZone(AEST); df2.setTimeZone(AEST);
     
     String[] cast1Array = {"actor 1", "actor 2"};
     String[] cast2Array = {"actor 1", "actor 2", "actor 3", "actor 4", "actor 5", "actor 6"};
@@ -44,10 +62,34 @@ public class MovieTest {
     movies.put(1, new Movie(1,"Mov 1",cast1,Classification.G,dir1,"synop 1",releaseDate.get(0)));
     movies.put(2, new Movie(2,"Mov 2",cast1,Classification.G,dir1,"synop 2",releaseDate.get(1)));
     movies.put(3, new Movie(3,"Mov 3",cast1,Classification.G,dir1,"synop 3",releaseDate.get(2)));
-    movies.put(4, new Movie(4,"Mov 4",cast2,Classification.G,dir1,"synop 4",releaseDate.get(3)));
-    movies.put(5, new Movie(5,"Mov 5",cast2,Classification.G,dir1,"synop 5",releaseDate.get(4)));
-    movies.put(6, new Movie(6,"Mov 6",cast2,Classification.G,dir1,"synop 6",releaseDate.get(5)));
+    movies.put(4, new Movie(4,"Mov 4",cast2,Classification.G,dir2,"synop 4",releaseDate.get(3)));
+    movies.put(5, new Movie(5,"Mov 5",cast2,Classification.G,dir2,"synop 5",releaseDate.get(4)));
+    movies.put(6, new Movie(6,"Mov 6",cast2,Classification.G,dir2,"synop 6",releaseDate.get(5)));
   
+
+    cinemas.put(1, new Cinema(1,MovieClass.BRONZE));
+    cinemas.put(2, new Cinema(1,MovieClass.SILVER));
+    cinemas.put(3, new Cinema(1,MovieClass.GOLD));
+
+    showingTime = new ArrayList<>();
+    for (int i = 0; i < 6; i++ ) {
+      showingTime.add(Calendar.getInstance(AEST, Locale.ENGLISH));
+      showingTime.get(i).add(Calendar.HOUR,24 + i);
+    }
+    Calendar inThePast = Calendar.getInstance(AEST, Locale.ENGLISH);
+
+    inThePast.setTimeInMillis(1635455293000L);
+    try {
+      showings.add(new Showing(1,movies.get(1),cinemas.get(1),showingTime.get(0)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(2),showingTime.get(1)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(2),showingTime.get(2)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(3),showingTime.get(3)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(3),showingTime.get(4)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(3),showingTime.get(5)));
+      showings.add(new Showing(1,movies.get(1),cinemas.get(3),inThePast));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Test void testGetterMethods() {
@@ -61,4 +103,31 @@ public class MovieTest {
     assertEquals("synop 1",movies.get(1).getSynopsis()); 
     assertEquals("2021-10-01",df.format(movies.get(1).getReleaseDate().getTime()));
   }
+
+  //TODO test null behavior.
+  @Test void testConstructor() {
+
+  }
+
+  @Test void testPrintMovieDetails() {
+    //public static ArrayList<Movie> printAllShowings(HashMap<Integer,Movie> movies) {
+
+    movies.get(1).addShowing(showings.get(0));
+    movies.get(1).addShowing(showings.get(1));
+    movies.get(1).addShowing(showings.get(2));
+    movies.get(1).addShowing(showings.get(6));
+    assertEquals(3,movies.get(1).printMovieShowings());
+
+
+    String expected = "-----------------------------------------\n" +
+    "ID  TIME                 CINEMA\n" +
+    "-----------------------------------------\n" +
+    "1   " + df2.format(showingTime.get(0).getTime()).toUpperCase() + "  1 - BRONZE CLASS\n" +
+    "2   " + df2.format(showingTime.get(1).getTime()).toUpperCase() + "  1 - SILVER CLASS\n" +
+    "3   " + df2.format(showingTime.get(2).getTime()).toUpperCase() + "  1 - SILVER CLASS\n\n";
+
+    assertEquals(expected,actualOut.toString());
+
+  }
+
 }
