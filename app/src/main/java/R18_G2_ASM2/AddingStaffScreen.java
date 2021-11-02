9cc8d6a0b0bec4086f28d806c945436329f05b99
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-02 20:51:28 +0800

    update the adding staff screen without integrating with the Manager Page

---
+++ app/src/main/java/R18_G2_ASM2/AddingStaffScreen.java
@@ -0,0 +1,22 @@
+package R18_G2_ASM2;
+
+public class AddingStaffScreen extends Screen{
+
+    private static User manager;
+
+    //the parameters there should be (ManagerScreen a,User user)
+    public AddingStaffScreen(){
+        super();
+
+
+    }
+    @Override
+    protected void chooseOption() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
