8b09a6328ad591e39e7e1a593d0f3bf5838eb58e
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-02 22:00:52 +0800

    add the adding staff screen

--- app/src/main/java/R18_G2_ASM2/AddingStaffScreen.java
+++ app/src/main/java/R18_G2_ASM2/AddingStaffScreen.java
@@ -1,22 +1,4 @@
 package R18_G2_ASM2;
 
-public class AddingStaffScreen extends Screen{
-
-    private static User manager;
-
-    //the parameters there should be (ManagerScreen a,User user)
-    public AddingStaffScreen(){
-        super();
-
-
-    }
-    @Override
-    protected void chooseOption() {
-
-    }
-
-    @Override
-    public void print() {
-
-    }
+public class AddingStaffScreen {
 }
