dbe7879aa930642ef728d52f360d4ae6593639e0
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-03 14:17:01 +0800

    fix the editing information method

--- app/src/main/java/R18_G2_ASM2/DeleteStaffScreen.java
+++ app/src/main/java/R18_G2_ASM2/DeleteStaffScreen.java
@@ -1,4 +1,23 @@
 package R18_G2_ASM2;
 
 public class DeleteStaffScreen extends Screen{
+
+    public DeleteStaffScreen(MovieSystem mSystem) {
+        super(mSystem);
+    }
+
+    @Override
+    protected void chooseOption() {
+
+    }
+
+    @Override
+    protected void setOptions() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
--- app/src/main/java/R18_G2_ASM2/EditStaffScreen.java
+++ app/src/main/java/R18_G2_ASM2/EditStaffScreen.java
@@ -15,27 +15,34 @@ public class EditStaffScreen extends Screen{
 
     //the parameters there should be (ManagerScreen a,User user)
     // user there should be a manager which is confirmed in the home page of staff?
-    public EditStaffScreen(Screen a,User user){
-        super();
-        this.superiorScreen = a;
-        this.manager = user;
-        this.title = "Editing STAFF";
-        options = new ArrayList<>();
-
-
+    public EditStaffScreen(MovieSystem mSystem) {
+        super(mSystem);
+        this.title = "Editing Staff";
+        this.maxInputInt =2;
     }
+
     @Override
     protected void chooseOption(){
+        if(intOption != NO_INT_OPTION){
 
+        }
     }
 
+    @Override
+    protected void setOptions() {
+        options = new ArrayList<>();
+        options.add("1");
+        options.add("2");
+    }
     @Override
     public void print(){
         clearScreen();
         printHeader();
-
+        System.out.println("Welcome %s\n",mSystem.getUser().getNickname());
         printOptionsText();
+        System.out.print(formatANSI("1",ANSI_USER_OPTION)+"Add a new staff\n");
+        System.out.print(formatANSI("2",ANSI_USER_OPTION)+"Delete a current staff\n");
+        System.out.print(formatANSI("Q",ANSI_USER_OPTION)+"back to manager home page\n");
     }
-
 
 }
