f8fd4a64e72da0123a97d2a90d95a0f90de40c66
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-03 14:58:34 +0800

    finish the editing user page

--- app/src/main/java/R18_G2_ASM2/AddingStaffScreen.java
+++ app/src/main/java/R18_G2_ASM2/AddingStaffScreen.java
@@ -2,5 +2,24 @@ package R18_G2_ASM2;
 
 import java.util.ArrayList;
 
-public class AddingStaffScreen {
+public class AddingStaffScreen extends Screen{
+
+    public AddingStaffScreen(MovieSystem mSystem) {
+        super(mSystem);
+    }
+
+    @Override
+    protected void chooseOption() {
+
+    }
+
+    @Override
+    protected void setOptions() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
