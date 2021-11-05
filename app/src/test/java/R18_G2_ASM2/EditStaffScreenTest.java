c087eab522e9fe7e9bcfe87de1ee2bcd6d89f5e5
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-05 15:20:48 +0800

    add EditStaffScreenTest.java

---
+++ app/src/test/java/R18_G2_ASM2/EditStaffScreenTest.java
@@ -0,0 +1,40 @@
+package R18_G2_ASM2;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+
+public class EditStaffScreenTest {
+    private static ByteArrayInputStream mockIn;
+    private static ByteArrayOutputStream actualOut;
+    private static MovieSystem mockMovieSystem;
+
+
+    @BeforeAll
+    static void setup() {
+        mockMovieSystem = mock(MovieSystem.class);
+        actualOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOut));
+    }
+
+    @BeforeEach
+    void resetSystem() {
+        actualOut.reset();
+    }
+
+    @Test
+    public void testConstructor() {
+        EditStaffScreen testEd = new EditStaffScreen(mockMovieSystem);
+        assertEquals("Editing Staff",testEd.title);
+    }
+
+}
