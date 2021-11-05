51924f29cf115aff4cab60d8dcda6a80c4fc3ba0
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-05 20:51:41 +0800

    add HomeScreenTest

---
+++ app/src/test/java/R18_G2_ASM2/HomeScreenTest.java
@@ -0,0 +1,48 @@
+package R18_G2_ASM2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class HomeScreenTest {
+    private static ByteArrayInputStream mockIn;
+    private static ByteArrayOutputStream actualOut;
+    private static MovieSystem mockMovieSystem;
+
+
+    @BeforeAll
+    static void setup() {
+        mockMovieSystem = mock(MovieSystem.class);
+        actualOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOut));
+    }
+
+    @BeforeEach
+    void resetSystem() {
+        actualOut.reset();
+    }
+
+    @Test
+    public void testConstructor() {
+        HomeScreen s =  new HomeScreen(mockMovieSystem);
+        assertEquals("CURRENT SHOWINGS (1 NOV - 7 NOV)",s.title);
+    }
+
+    @Test public void testInvalidUserInputSimpleInvalidIntegers() {
+        mockIn = new ByteArrayInputStream("q\n".getBytes());
+        System.setIn(mockIn);
+
+        HomeScreen s =  new HomeScreen(mockMovieSystem);
+
+        s.askforInput();
+        Assertions.assertEquals(actualOut.toString(),"SEE YOU NEXT TIME! :)\n");
+    }
+}
