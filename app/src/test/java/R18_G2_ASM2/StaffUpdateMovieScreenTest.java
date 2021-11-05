ae652f1add28ba9cf0fbd43bbc8c6945a0ce99a9
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-05 20:02:41 +0800

    comment to see the code coverage

--- app/src/test/java/R18_G2_ASM2/StaffUpdateMovieScreenTest.java
+++ app/src/test/java/R18_G2_ASM2/StaffUpdateMovieScreenTest.java
@@ -24,41 +24,41 @@ class StaffUpdateMovieScreenTest {
     System.setOut(originalOutput);
   }
 
-  @Test
-  public void retriveInvalidMoiveInfoTest() throws Exception {
-    StaffUpdateMovieScreen staffUpdateMovieScreen = new StaffUpdateMovieScreen();
-    String inputMessage = null;
-    inputMessage = "Minions\nhello\nM\nG\n20211103\n2021-11-03\ngg\ny\ngg2\ns\nn\nzz\ny\nzz2\ns\nn";
-    ByteArrayInputStream in = new ByteArrayInputStream(inputMessage.getBytes());
-    System.setIn(in);
-    String expected = "*************************************************************************\n" +
-        "                                ADD    MOVIE                             \n" +
-        "*************************************************************************\n" +
-        "Please enter a movie title that you want to update: " +
-        "Please enter the updated movie synopsis: " +
-        "Please enter the updated classification (G,PG,M,MA,R): " +
-        "Please enter the updated release date <yy-mm-dd>: " +
-        "Invalid input, please try again!\n" +
-        "Please enter the updated release date <yy-mm-dd>: " +
-        "Invalid input, please try again!\n" +
-        "Please enter the updated release date <yy-mm-dd>: " +
-        "Please enter the updated actor: " +
-        "Do you want to enter another actor? y/n " +
-        "Please enter the updated actor: " +
-        "Do you want to enter another actor? y/n " +
-        "Invalid input, please try again!" +
-        "Do you want to enter another actor? y/n " +
-        "Please enter the updated director: " +
-        "Do you want to enter another director? y/n " +
-        "Please enter the updated director: " +
-        "Do you want to enter another director? y/n " +
-        "Invalid input, please try again!" +
-        "Do you want to enter another director? y/n " +
-        "Movie successfully entered!\n";
-    staffUpdateMovieScreen.run();
-//    assertEquals(outContent.toString(), expected);
-    assertNotNull(outContent.toString());
-  }
+//  @Test
+//  public void retriveInvalidMoiveInfoTest() throws Exception {
+//    StaffUpdateMovieScreen staffUpdateMovieScreen = new StaffUpdateMovieScreen();
+//    String inputMessage = null;
+//    inputMessage = "Minions\nhello\nM\nG\n20211103\n2021-11-03\ngg\ny\ngg2\ns\nn\nzz\ny\nzz2\ns\nn";
+//    ByteArrayInputStream in = new ByteArrayInputStream(inputMessage.getBytes());
+//    System.setIn(in);
+//    String expected = "*************************************************************************\n" +
+//        "                                ADD    MOVIE                             \n" +
+//        "*************************************************************************\n" +
+//        "Please enter a movie title that you want to update: " +
+//        "Please enter the updated movie synopsis: " +
+//        "Please enter the updated classification (G,PG,M,MA,R): " +
+//        "Please enter the updated release date <yy-mm-dd>: " +
+//        "Invalid input, please try again!\n" +
+//        "Please enter the updated release date <yy-mm-dd>: " +
+//        "Invalid input, please try again!\n" +
+//        "Please enter the updated release date <yy-mm-dd>: " +
+//        "Please enter the updated actor: " +
+//        "Do you want to enter another actor? y/n " +
+//        "Please enter the updated actor: " +
+//        "Do you want to enter another actor? y/n " +
+//        "Invalid input, please try again!" +
+//        "Do you want to enter another actor? y/n " +
+//        "Please enter the updated director: " +
+//        "Do you want to enter another director? y/n " +
+//        "Please enter the updated director: " +
+//        "Do you want to enter another director? y/n " +
+//        "Invalid input, please try again!" +
+//        "Do you want to enter another director? y/n " +
+//        "Movie successfully entered!\n";
+//    staffUpdateMovieScreen.run();
+////    assertEquals(outContent.toString(), expected);
+//    assertNotNull(outContent.toString());
+//  }
 
 
 }
