45afc7625d48d1d0fd9afde020a7479da8cb8847
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-11-05 17:35:26 +0800

    @test

---
+++ app/src/test/java/R18_G2_ASM2/DeleteStaffTest.java
@@ -0,0 +1,42 @@
+package R18_G2_ASM2;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class DeleteStaffTest {
+    private static ByteArrayInputStream mockIn;
+    private static ByteArrayOutputStream actualOut;
+    private static MovieSystem mockMovieSystem;
+
+
+    @BeforeAll
+    static void setup() {
+        mockMovieSystem = mock(MovieSystem.class);
+        actualOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOut));
+    }
+
+
+    @BeforeEach
+    void resetSystem() {
+        actualOut.reset();
+    }
+
+    @Test
+    void testAskForEmailExist(){
+        mockIn = new ByteArrayInputStream("hp@gmail.com\n".getBytes());
+        System.setIn(mockIn);
+        DeleteStaffScreen ds =  new DeleteStaffScreen(mockMovieSystem);
+        ds.run();
+        assertEquals("The user input is not a staff\nPlease try again.\n",actualOut.toString());
+    }
+
+}
